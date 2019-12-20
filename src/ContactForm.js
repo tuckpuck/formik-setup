461cc4d64d502373d8708dc02b4263a23e565ebb
tuckpuck <tucker.triggs@yahoo.com>
2019-12-20 09:52:21 -0700

    add demo from formspree website

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,43 +1,64 @@
-// Render Prop
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const ContactForm = () => (
-  <div>
-    <h1>Any place in your app!</h1>
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+const formSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Required"),
+  message: Yup.string().required("Required")
+});
 
-export default ContactForm;
+export default () => {
+  /* Server State Handling */
+  const [serverState, setServerState] = useState();
+  const handleServerResponse = (ok, msg) => {
+    setServerState({ ok, msg });
+  };
+  const handleOnSubmit = (values, actions) => {
+    axios({
+      method: "POST",
+      url: "http://formspree.io/YOUR_FORM_ID",
+      data: values
+    })
+      .then(response => {
+        actions.setSubmitting(false);
+        actions.resetForm();
+        handleServerResponse(true, "Thanks!");
+      })
+      .catch(error => {
+        actions.setSubmitting(false);
+        handleServerResponse(false, error.response.data.error);
+      });
+  };
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <Formik
+        initialValues={{ email: "", message: "" }}
+        onSubmit={handleOnSubmit}
+        validationSchema={formSchema}
+      >
+        {({ isSubmitting }) => (
+          <Form id="fs-frm" noValidate>
+            <label htmlFor="email">Email:</label>
+            <Field id="email" type="email" name="email" />
+            <ErrorMessage name="email" className="errorMsg" component="p" />
+            <label htmlFor="message">Message:</label>
+            <Field id="message" name="message" component="textarea" />
+            <ErrorMessage name="message" className="errorMsg" component="p" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+            {serverState && (
+              <p className={!serverState.ok ? "errorMsg" : ""}>
+                {serverState.msg}
+              </p>
+            )}
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
